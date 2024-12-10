09b0e1968787dc0c75f390127eeb1dfd2c3a951d
albertjseo <albertjseo@gmail.com>
2024-12-10 07:50:06 -0800

    Upodating Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import {Row, Col, TabContainer, TabContent, TabPane} from "react-bootstrap";
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import { ProjectCard } from "./ProjectCard";
+import projImg0 from "../assets/img/projImg0.png"
 import projImg1 from "../assets/img/projImg1.png"
 import projImg2 from "../assets/img/projImg2.png"
 import projImg3 from "../assets/img/projImg3.png"
@@ -15,32 +16,37 @@ export default function Projects() {
 
     const projectInfo = [
         {
-          title: "Cell Morphology Tracker",
+            title: <a href="https://github.com/albertjseo/viral-integration-simulation" target="_blank">Viral Integration Simulation - On Going</a>,
+          description: "Python, Snakemake",
+          imgUrl: projImg0,
+        },
+        {
+            title: <a href="https://github.com/albertjseo/cell-image-processing" target="_blank">Cell Morphology Tracker </a>,
           description: "MATLAB",
           imgUrl: projImg1,
         },
         {
-          title: "Covid-19 with SQL",
+            title: <a href="https://github.com/albertjseo/covid19-analysis-with-sql" target="_blank">Covid-19 with SQL </a>,
           description: "Python, HTML, CSS, SQL, Flask",
           imgUrl: projImg2,
         },
         {
-          title: "Drug Discovery",
+            title: <a href="https://github.com/albertjseo/drug-discovery" target="_blank">Drug Discovery </a>,
           description: "Python",
           imgUrl: projImg3,
         },
         {
-          title: "Custom IgBLAST Database",
+            title: <a href="https://github.com/albertjseo/agm_db" target="_blank">Custom IgBLAST Database</a>,
           description: "Python",
           imgUrl: projImg4,
         },
         {
-          title: "Valorant Tracker",
+            title: <a href="https://github.com/albertjseo/valorant-tracker" target="_blank">Valorant Tracker</a>,
           description: "Python, HTML, CSS, Flask, API",
           imgUrl: projImg5,
         },
         {
-          title: "Simple Survey",
+            title: <a href="https://github.com/albertjseo/survey_sample" target="_blank">Simple Survey</a>,
           description: "JavaScript, React, CSS, HTML",
           imgUrl: projImg6,
         }
