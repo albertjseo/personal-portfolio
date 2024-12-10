74701f3f178581dda16e4fd054a83a39f03881c4
albertjseo <albertjseo@gmail.com>
2024-12-10 07:52:26 -0800

    Upodating Projects.js text color

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,37 +16,37 @@ export default function Projects() {
 
     const projectInfo = [
         {
-            title: <a href="https://github.com/albertjseo/viral-integration-simulation" target="_blank">Viral Integration Simulation - On Going</a>,
+            title: <a href="https://github.com/albertjseo/viral-integration-simulation" target="_blank" className="project--title">Viral Integration Simulation - On Going</a>,
           description: "Python, Snakemake",
           imgUrl: projImg0,
         },
         {
-            title: <a href="https://github.com/albertjseo/cell-image-processing" target="_blank">Cell Morphology Tracker </a>,
+            title: <a href="https://github.com/albertjseo/cell-image-processing" target="_blank" className="project--title">Cell Morphology Tracker </a>,
           description: "MATLAB",
           imgUrl: projImg1,
         },
         {
-            title: <a href="https://github.com/albertjseo/covid19-analysis-with-sql" target="_blank">Covid-19 with SQL </a>,
+            title: <a href="https://github.com/albertjseo/covid19-analysis-with-sql" target="_blank" className="project--title">Covid-19 with SQL </a>,
           description: "Python, HTML, CSS, SQL, Flask",
           imgUrl: projImg2,
         },
         {
-            title: <a href="https://github.com/albertjseo/drug-discovery" target="_blank">Drug Discovery </a>,
+            title: <a href="https://github.com/albertjseo/drug-discovery" target="_blank" className="project--title">Drug Discovery </a>,
           description: "Python",
           imgUrl: projImg3,
         },
         {
-            title: <a href="https://github.com/albertjseo/agm_db" target="_blank">Custom IgBLAST Database</a>,
+            title: <a href="https://github.com/albertjseo/agm_db" target="_blank" className="project--title">Custom IgBLAST Database</a>,
           description: "Python",
           imgUrl: projImg4,
         },
         {
-            title: <a href="https://github.com/albertjseo/valorant-tracker" target="_blank">Valorant Tracker</a>,
+            title: <a href="https://github.com/albertjseo/valorant-tracker" target="_blank" className="project--title">Valorant Tracker</a>,
           description: "Python, HTML, CSS, Flask, API",
           imgUrl: projImg5,
         },
         {
-            title: <a href="https://github.com/albertjseo/survey_sample" target="_blank">Simple Survey</a>,
+            title: <a href="https://github.com/albertjseo/survey_sample" target="_blank" className="project--title">Simple Survey</a>,
           description: "JavaScript, React, CSS, HTML",
           imgUrl: projImg6,
         }
