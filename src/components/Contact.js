26092726893bae33037051880390af6077dcee69
albertjseo <albertjseo@gmail.com>
2024-12-12 15:53:57 -0800

    Updating Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import Container from "react-bootstrap/Container";
 import {Col, Row} from "react-bootstrap";
-import contactImg from "../assets/img/contact-img.svg"
 export const Contact = () => {
     const formInitialDetails = {
         firstName: '',
@@ -20,18 +19,31 @@ export const Contact = () => {
         })
     }
 
-    const submit = () => {
-
-    }
+    const submit = async (e) => {
+        e.preventDefault();
+        setButtonText("Sending...")
+        let response = await fetch("http://localhost:3000/contact", {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json;charset=utf-8",
+            },
+            body: JSON.stringify(formDetails)
+        });
+        setButtonText("Send");
+        let result = response.json()
+        setFormDetails(formInitialDetails)
+        if (result.code === 200) {
+            setStatus({ success: true, message: "Message Sent!"})
+        } else {
+            setStatus({ success: false, message: "Message did not send. Please try again later"})
+        }
+    };
 
     return (
         <section className="contact" id="connect">
             <Container>
-                <Row>
-                    <Col md={6}>
-                        <img src={contactImg} alt="Contact Us" />
-                    </Col>
-                    <Col md={6}>
+                <Row className="align-items-center">
+                    <Col>
                         <h2>Get In Touch</h2>
                         <form onSubmit={submit}>
                             <Row>
@@ -48,7 +60,7 @@ export const Contact = () => {
                                            onChange={(e) => onFormUpdate('email', e.target.value)}/>
                                 </Col>
                                 <Col>
-                                    <textarea row="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)} />
+                                    <textarea row="10" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)} />
                                     <button type="submit"><span>{buttonText}</span></button>
                                 </Col>
                                 {
