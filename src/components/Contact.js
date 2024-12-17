87a77b614e60f2971ab0ddb5e1bf94e191e1556e
albertjseo <albertjseo@gmail.com>
2024-12-16 20:00:19 -0800

    Updating Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -59,7 +59,7 @@ export const Contact = () => {
                                     <input type="email" value={formDetails.email} placeholder="Email"
                                            onChange={(e) => onFormUpdate('email', e.target.value)}/>
                                 </Col>
-                                <Col>
+                                <Col sm={6} className="px-1">
                                     <textarea row="10" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)} />
                                     <button type="submit"><span>{buttonText}</span></button>
                                 </Col>
