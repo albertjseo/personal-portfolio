7ec2447f681a2ce3150dc8a9c0247f5dfd7eac60
albertjseo <albertjseo@gmail.com>
2025-02-07 21:57:59 -0800

    Updating NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -46,7 +46,7 @@ export default function NavBar() {
                 <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                 <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
                 <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-                <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('publications')}>Publications</Nav.Link>
+                <Nav.Link href="#publications" className={activeLink === 'publications' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('publications')}>Publications</Nav.Link>
 
               </Nav>
               <span className="navbar-text">
