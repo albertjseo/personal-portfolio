db6911529352f2252d9891550f851b5e6f98fb18
albertjseo <albertjseo@gmail.com>
2025-05-08 06:52:26 -0700

    Updating Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,7 +22,7 @@ export const Contact = () => {
     const submit = async (e) => {
         e.preventDefault();
         setButtonText("Sending...")
-        let response = await fetch("/api/server", {
+        let response = await fetch("https://albertjseo.com/api/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "Application/json;charset=utf-8",
