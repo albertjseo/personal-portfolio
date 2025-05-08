848db846bc3ee672feb9e69bbf48065fc87c4c6c
albertjseo <albertjseo@gmail.com>
2025-05-08 07:17:06 -0700

    Updating server.js and Contact.js

--- api/server.js
+++ api/server.js
@@ -8,8 +8,8 @@ export default async function handler(req, res) {
 
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // TLS mode for Gmail
+        port: 587, // TLS mode for Gmail
+        secure: false, // false = TLS (not SSL)
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
