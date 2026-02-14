a699e496ea26d2f9f9864c7ca53151d5a53ee64b
albertjseo <albertjseo@gmail.com>
2026-02-14 10:49:22 -0800

    Updating App.css and Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
-import { Row, Col, TabContainer, TabContent, TabPane } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import React from "react";
-import Nav from "react-bootstrap/Nav";
 import { ProjectCard } from "./ProjectCard";
 import Container from "react-bootstrap/Container";
 
@@ -196,30 +195,14 @@ export default function Projects() {
           <Col>
             <h2>PROJECTS</h2>
 
-            <TabContainer id="project-tabs" defaultActiveKey="first">
-              <Nav
-                variant="pills"
-                className="nav-pills mb-5 justify-content-center align-items-center"
-                id="pills-tab"
-              >
-                <Nav.Item>
-                  <Nav.Link eventKey="first">For More Information</Nav.Link>
-                </Nav.Item>
-              </Nav>
+            {/* Divider */}
+            <div className="project-divider"></div>
 
-              <TabContent>
-                <TabPane eventKey="first">
-                  <Row>
-                    {projectInfo.map((project, index) => (
-                      <ProjectCard key={index} {...project} />
-                    ))}
-                  </Row>
-                </TabPane>
-
-                <TabPane eventKey="second">TEXT</TabPane>
-                <TabPane eventKey="third">TEXT</TabPane>
-              </TabContent>
-            </TabContainer>
+            <Row>
+              {projectInfo.map((project, index) => (
+                <ProjectCard key={index} {...project} />
+              ))}
+            </Row>
           </Col>
         </Row>
       </Container>
