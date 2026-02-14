154028cb9abd6b48412d4f602a7864721f12baaa
albertjseo <albertjseo@gmail.com>
2026-02-14 09:47:31 -0800

    Updating Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -23,15 +23,15 @@ export default function NavBar() {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 5
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 3
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
+      items: 3
     }
   };
 
